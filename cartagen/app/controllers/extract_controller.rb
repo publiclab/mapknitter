4db84ac8183ee224a57b4ed03ea55ea006954061
jywarren <warren@mit.edu>
2009-05-20 17:31:06 +0000

    safari works!!!!!! lots of new stuff too.

---
+++ cartagen/app/controllers/extract_controller.rb
@@ -0,0 +1,28 @@
+class ExtractController < ApplicationController
+    caches_page :osm_to_json_by_tag
+
+    def osm_to_json
+      params[:url] ||= "http://localhost:3000/rome.osm"
+      @features = ParseOsm.parse(params[:url])
+      puts @features.length
+      respond_to do |format|
+        format.html { render :html => @features }
+        format.xml  { render :xml => @features }
+        format.kml  { render :template => "map/plot.kml.erb" }
+        format.js  { render :json => @features }
+      end
+    end
+
+    def osm_to_json_by_tag
+      params[:url] ||= "http://localhost:3000/rome.osm"
+      @features = ParseOsm.filter(params[:url],params[:tag])
+      puts @features.length
+      respond_to do |format|
+        format.html { render :html => @features }
+        format.xml  { render :xml => @features }
+        format.kml  { render :template => "map/plot.kml.erb" }
+        format.js  { render :json => @features }
+      end
+    end
+
+end
