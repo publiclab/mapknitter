e4903a2c53542c313c56cc4a55be21bc73bae77a
benweissmann <benweissmann@gmail.com>
2009-06-08 15:46:10 +0000

    rename iphone to mushroommap

---
+++ phonegap-clients/phonegap/Classes/Map.m
@@ -0,0 +1,18 @@
+//
+//  Map.m
+//  MushroomMap
+//
+//  Created by Brant Vasilieff on 3/4/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "Map.h"
+
+
+@implementation Map
+
+- (void)open:(NSArray*)arguments withDict:(NSDictionary*)options
+{
+}
+
+@end
