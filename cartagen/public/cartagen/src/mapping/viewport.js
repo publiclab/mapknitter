c9c8159b76c260f8a45c5e752ba3d9b7aecd2824
Jeffrey Warren <warren@mit.edu>
2009-07-07 21:17:52 +0000

    coastline drawing!!!!!

--- cartagen/public/cartagen/src/mapping/viewport.js
+++ cartagen/public/cartagen/src/mapping/viewport.js
@@ -6,7 +6,7 @@ var Viewport = {
 	 * Frame to show bbox culling
 	 * @type Number
 	 */
-	padding: 100,
+	padding: 0,
 	/**
 	 * Bbox in [y1, x1, y2, x2] format
 	 * @type Number[]
@@ -17,7 +17,7 @@ var Viewport = {
 	 * @type Array[]
 	 */
 	full_bbox: function() {
-		return [this.bbox[1],this.bbox[0]],[this.bbox[3],this.bbox[0]],[this.bbox[3],this.bbox[2]],[this.bbox[1],this.bbox[2]]
+		return [[this.bbox[1],this.bbox[0]],[this.bbox[3],this.bbox[0]],[this.bbox[3],this.bbox[2]],[this.bbox[1],this.bbox[2]]]
 	},
 	/**
 	 * X-width of the viewport
@@ -29,6 +29,31 @@ var Viewport = {
 	 */
 	height: 0,
 	/**
+	 * Yields the x,y of the nearest Viewport corner in an array as [x,y,corner] where corner is 0,1,2,3 clockwise from top left
+	 * 
+	 * @param {Number} x  x-coordinate of query
+	 * @param {Number} y  y-coordinate of query
+	 */
+	nearest_corner: function(x,y) {
+		var corner = []
+		if (Math.abs(Viewport.bbox[1] - x) < Math.abs(Viewport.bbox[3] - x)) {
+			corner[0] = Viewport.bbox[1]
+			corner[2] = 0
+		} else {
+			corner[0] = Viewport.bbox[3]
+			corner[2] = 1
+		}
+		if (Math.abs(Viewport.bbox[0] - y) < Math.abs(Viewport.bbox[2] - y)) {
+			corner[1] = Viewport.bbox[0]
+		} else {
+			corner[1] = Viewport.bbox[2]
+			corner[2] -= 1
+			corner[2] *= -1 // swap 1 and 0
+			corner[2] += 2
+		}
+		return corner
+	},
+	/**
 	 * Varies around 1.0 as function of hardware resolution. This assumes that resolution is
 	 * somewhat proportianal to power, which is generally true. This should eventually be replaced
 	 * with something that measures initial load speed in fps to get a better estimate.
@@ -37,4 +62,17 @@ var Viewport = {
 	power: function() {
 		return window.screen.width/1024
 	},
+	/**
+	 * Performs per-frame setup functions such as resizing the viewport to match the containing 
+	 * window or element. Also generates Viewport.bbox
+	 */
+	draw: function() {
+		Viewport.width = Glop.width * (1 / Cartagen.zoom_level) - (2 * Viewport.padding * (1 / Cartagen.zoom_level))
+        Viewport.height = Glop.height * (1 / Cartagen.zoom_level) - (2 * Viewport.padding * (1 / Cartagen.zoom_level))
+        // culling won't work anymore after we fixed rotation... everything's got to be square.
+        Viewport.width = Math.max(Viewport.width, Viewport.height)
+        Viewport.height = Viewport.width
+        Viewport.bbox = [Map.y - Viewport.height / 2, Map.x - Viewport.width / 2, Map.y + Viewport.height / 2, Map.x + Viewport.width / 2]
+        // $C.stroke_rect(Map.x-Viewport.width/2,Map.y-Viewport.height/2,Viewport.width,Viewport.height)
+	}
 }
