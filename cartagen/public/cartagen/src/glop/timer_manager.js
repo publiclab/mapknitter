d19ec0ba7f56d15049afd31d4f6fa69cdef65b59
Jeffrey Warren <warren@mit.edu>
2009-07-20 18:10:26 +0000

    basic timer functionality

--- cartagen/public/cartagen/src/glop/timer_manager.js
+++ cartagen/public/cartagen/src/glop/timer_manager.js
@@ -1,31 +1,35 @@
 var TimerManager = {
-	date: new Date,
+	last_date: new Date,
+	lags: [],
 	setup: function(f,i) {
 		this.f = f || Prototype.emptyFunction
-		setTimeout(this.f,i || 10)
+		setTimeout(this.bound_run,i || 10)
+	},
+	bound_run: function() {
+		TimerManager.run.apply(TimerManager)
 	},
 	run: function() {
 		var new_date = new Date
 		var lag = new_date - this.last_date
 		this.last_date = new_date
-		this.history.unshift(lag)
-		
+		this.lags.unshift(lag)
+		if (this.lags.length > 100) this.lags.pop()
+		this.f()
 		this.interval = this.sample()
-		setTimeout(this.f,this.interval)
+		setTimeout(this.bound_run,this.interval)
 	},
 	sample: function() {
 		var sample = 0
-		sample += this.history[1] || 0
-		sample += this.history[2] || 0
-		sample += this.history[3] || 0
-		sample += this.history[5] || 0
-		sample += this.history[8] || 0
-		sample += this.history[13] || 0
-		sample += this.history[21] || 0
-		sample += this.history[34] || 0
-		sample += this.history[55] || 0
+		sample += this.lags[1] || 0
+		sample += this.lags[2] || 0
+		sample += this.lags[3] || 0
+		sample += this.lags[5] || 0
+		sample += this.lags[8] || 0
+		sample += this.lags[13] || 0
+		sample += this.lags[21] || 0
+		sample += this.lags[34] || 0
+		sample += this.lags[55] || 0
 		return sample/9
 	},
-	history: [],
 }
 // TimerManager.setup(f)
