d8d09263d60af2f691e761b73b731d8a1f0bdcd8
Jeffrey Warren <warren@mit.edu>
2009-07-21 21:09:32 +0000

    fixed lags>intervals, was using the lag instead of the interval as TimerManager.interval

--- cartagen/public/cartagen/src/glop/timer_manager.js
+++ cartagen/public/cartagen/src/glop/timer_manager.js
@@ -1,11 +1,20 @@
-// TimerManager.setup(f)
-
-// what if we close the laptop... 
-
+/**
+ * @namespace Manages CPU usage by running main loop process,
+ * monitoring loop completion, and adjusting interval to compensate.
+ */
 var TimerManager = {
+	/**
+	 * Date of last execution
+	 */
 	last_date: new Date,
-	lags: [],
-	spacing: 2,
+	/**
+	 * Intervals of last 100 executions. 
+	 */
+	intervals: [],
+	/**
+	 * Factor by which to space out executions. 2 means run them 
+	 */
+	spacing: 3,
 	interval: 10,
 	setup: function(f,i) {
 		this.f = f || Prototype.emptyFunction
@@ -17,22 +26,21 @@ var TimerManager = {
 	},
 	run: function() {
 		var new_date = new Date
-			// $l('> '+(new_date - this.last_date))
-		var lag = Math.min(300,(((new_date - this.last_date) - this.interval) || 0))
-		// $l(lag)
+		// var lag = Math.min(300,(((new_date - this.last_date) - this.interval) || 0))
+		var measured_interval = ((new_date - this.last_date) - this.interval)
+		measured_interval = Math.max(measured_interval,10)
 		this.last_date = new_date
 		this.f()
-		this.lags.unshift(lag)
-		if (this.lags.length > 100) this.lags.pop()
+		this.intervals.unshift(parseInt(measured_interval))
+		if (this.intervals.length > 100) this.intervals.pop()
 		this.interval = this.sample()*this.spacing
-			// $l(parseInt(this.sample())+';'+this.interval)
 		setTimeout(this.bound_run,this.interval)
 	},
 	sample: function() {
 		var sample = 0
 		var sequence = [1,2,3,5,8,13,21,34,55]
 		for (var i = 0;i < sequence.length;i++) {
-			var add = this.lags[sequence[i]] || 0
+			var add = this.intervals[sequence[i]] || 0
 			// add = Math.min(add,1000)
 			sample += add
 		}
