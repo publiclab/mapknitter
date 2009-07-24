089df0692eae3f132ef5736e7d6d9931b697485f
benweissmann <benweissmann@gmail.com>
2009-07-24 21:29:31 +0000

    "prevent powersave mode while parsing features"

--- cartagen/public/cartagen/src/glop/glop.js
+++ cartagen/public/cartagen/src/glop/glop.js
@@ -116,7 +116,7 @@ var Glop = {
 	 */
 	draw_powersave: function() {
 		var delay = 20
-		if (this.tail > 0 || Config.powersave == false || (Cartagen.requested_plots && Cartagen.requested_plots > 0) || Cartagen.last_loaded_geohash_frame > Glop.frame-delay) {
+		if (this.tail > 0 || Config.powersave == false || (Cartagen.requested_plots && Cartagen.requested_plots > 0) || Cartagen.last_loaded_geohash_frame > Glop.frame-delay || Cartagen.parse_manager.completed < 100) {
 			if (this.tail > 0) this.tail -= 1
 			Glop.draw()
 		} //else $l('powersave: '+this.tail)
