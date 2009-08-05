43fa7f15562242b6647fb8ea893671b582d1af51
Jeffrey Warren <warren@mit.edu>
2009-08-05 20:21:52 +0000

    duplicate gem declaration

--- cartagen/config/environment.rb
+++ cartagen/config/environment.rb
@@ -77,6 +77,5 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-  
-  config.gem "geokit"
+
 end
