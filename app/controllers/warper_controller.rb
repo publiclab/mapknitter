79dcdcb7c1171b508d5681b5b567107bf7b71caf
Jeffrey Warren <warren@mit.edu>
2010-02-28 22:05:47 -0500

    image uploading via attachment_fu for warper

--- app/controllers/warper_controller.rb
+++ app/controllers/warper_controller.rb
@@ -6,12 +6,22 @@ class WarperController < ApplicationController
 
   def create
     @warpable = Warpable.new(params[:warpable])
+    puts params[:warpable]
     if @warpable.save
       flash[:notice] = 'Warpable was successfully created.'
-      # redirect_to mugshot_url(@warpable)     
+      redirect_to :action => :show, :id => @warpable.id
     else
+      flash[:notice] = 'There was an error'
       render :action => :new
     end
+  end
+  
+  def show
+    @image = Warpable.find params[:id]
+  end
+  
+  def list
+    @warpables = Warpable.find :all, :conditions => ['parent_id is NULL']
   end
   
 end
--- app/models/warpable.rb
+++ app/models/warpable.rb
@@ -2,10 +2,34 @@ class Warpable < ActiveRecord::Base
 
   has_attachment :content_type => :image, 
                  :storage => :s3, 
-                 :max_size => 3.megabytes,
+                 :max_size => 5.megabytes,
                  # :resize_to => '320x200>',
                  :thumbnails => { :medium => '500x375', :small => '240x180', :thumb => '100x100>' }
 
-  validates_as_attachment
+  # validates_as_attachment
+
+  def validate
+    errors.add_to_base("You must choose a file to upload") unless self.filename
+    
+    unless self.filename == nil
+      
+      # Images should only be GIF, JPEG, or PNG
+      [:content_type].each do |attr_name|
+        enum = attachment_options[attr_name]
+        unless enum.nil? || enum.include?(send(attr_name))
+          errors.add_to_base("You can only upload images (GIF, JPEG, or PNG)")
+        end
+      end
+      
+      # Images should be less than 5 MB
+      [:size].each do |attr_name|
+        enum = attachment_options[attr_name]
+        unless enum.nil? || enum.include?(send(attr_name))
+          errors.add_to_base("Images should be smaller than 5 MB in size")
+        end
+      end
+        
+    end
+  end
 
 end
