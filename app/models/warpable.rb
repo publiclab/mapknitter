3b51eb6cc5bebc1b95ae544796239f627dfe903a
Information Ecology <warren@mit.edu>
2010-06-18 12:08:27 -0400

    durnit i keep committing this... undoing

--- app/models/warpable.rb
+++ app/models/warpable.rb
@@ -3,8 +3,8 @@ require "ftools"
 class Warpable < ActiveRecord::Base
   
   has_attachment :content_type => :image, 
-		 :storage => :file_system,:path_prefix => 'public/warpables', 
-                 #:storage => :s3, 
+		 #:storage => :file_system,:path_prefix => 'public/warpables', 
+                 :storage => :s3, 
                  :max_size => 5.megabytes,
                  # :resize_to => '320x200>',
 		:processor => :image_science,
