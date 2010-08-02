14b83a9e6e6fa3cd3373be4c684c3c606ee95b2b
jywarren <warren@mit.edu>
2010-08-02 11:07:06 +0400

    fixed status messages

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,3 @@
-include ActionView::Helpers::NumberHelper
 class MapController < ApplicationController
   caches_page :find
 
@@ -208,11 +207,10 @@ class MapController < ApplicationController
   def output
 	@map = Map.find params[:id] 
 	if @export = Export.find_by_map_id(params[:id])
-		@running = (@export.status != 'complete' && @export.status != 'none')
+		@running = (@export.status != 'complete' && @export.status != 'none' && @export.status != 'failed')
 	else
 		@running = false
 	end
-	@size = number_to_human_size(@export.size) if @export
 	render :layout => false
   end
 
@@ -228,8 +226,10 @@ class MapController < ApplicationController
 			output = 'complete'
 		elsif export.status == 'none'
 			output = 'export has not been run'
+		elsif export.status == 'failed'
+			output = 'export failed'
 		else
-			output = export.status+' <img class="export_status" src="images/spinner.gif">'
+			output = export.status+' <img class="export_status" src="/images/spinner-small.gif">'
 		end
 	else
 		output = 'export has not been run'
@@ -238,53 +238,56 @@ class MapController < ApplicationController
   end
 
   def export
-    respond_to do |format|
-      format.html { 
 	map = Map.find_by_name params[:id]
+	begin
 
-	unless export = Export.find_by_map_id(map.id)
-		export = Export.new({:map_id => map.id,:status => 'starting'})
+		unless export = Export.find_by_map_id(map.id)
+			export = Export.new({:map_id => map.id,:status => 'starting'})
+		end
+		export.geotiff = true
+		export.status = 'starting'
+		export.save       
+	 
+		directory = "public/warps/"+map.name+"/"
+	    	`rm -r #{directory}`
+	
+		puts '> averaging scales'
+		pxperm = map.average_scale # pixels per meter
+	
+		puts '> distorting warpables'
+		origin = map.distort_warpables(pxperm)
+		warpable_coords = origin.pop	
+		
+		export = Export.find_by_map_id(map.id)
+		export.status = 'compositing'
+		export.save
+	
+		puts '> generating composite tiff'
+		geotiff_location = map.generate_composite_tiff(warpable_coords,origin)
+	
+		info = (`identify -quiet -format '%b,%w,%h' #{geotiff_location}`).split(',')
+	
+		export = Export.find_by_map_id(map.id)
+		export.size = info[0]
+		export.width = info[1]
+		export.height = info[2]
+		export.cm_per_pixel = 100.0000/pxperm
+		export.status = 'tiling'
+		export.save
+	
+		puts '> generating tiles'
+		export = Export.find_by_map_id(map.id)
+		export.tms = true if map.generate_tiles
+		export.status = 'complete'
+		export.save
+	
+		render :text => '<a href="/warps/'+map.name+'/'+map.name+'-geo.tif">'+map.name+'-geo.tif</a>'
+	rescue
+		export = Export.find_by_map_id(map.id)
+		export.status = 'failed'
+		export.save
 	end
-	export.geotiff = true
-	export.status = 'starting'
-	export.save       
- 
-	directory = "public/warps/"+map.name+"/"
-    	`rm -r #{directory}`
-
-	puts '> averaging scales'
-	pxperm = map.average_scale # pixels per meter
-
-	puts '> distorting warpables'
-	origin = map.distort_warpables(pxperm)
-	warpable_coords = origin.pop	
-
-	export = Export.find_by_map_id(map.id)
-	export.status = 'compositing'
-	export.save
-
-	puts '> generating composite tiff'
-	geotiff_location = map.generate_composite_tiff(warpable_coords,origin)
-
-	info = (`identify -quiet -format '%b,%w,%h' #{geotiff_location}`).split(',')
-
-	export = Export.find_by_map_id(map.id)
-	export.size = info[0]
-	export.width = info[1]
-	export.height = info[2]
-	export.cm_per_pixel = 100.0000/pxperm
-	export.status = 'tiling'
-	export.save
-
-	puts '> generating tiles'
-	export = Export.find_by_map_id(map.id)
-	export.tms = true if map.generate_tiles
-	export.status = 'complete'
-	export.save
-
-	render :text => '<a href="/warps/'+map.name+'/'+map.name+'-geo.tif">'+map.name+'-geo.tif</a>'
-      }
-    end
+   return :text => 'started export'
   end
 
 end
