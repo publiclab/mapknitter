db771e7fb9a49c08801a8b0b54814235ca685885
jywarren <jeff@unterbahn.com>
2011-06-30 09:35:16 -0400

    password log filtering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
   
 end
