70f591da0c8871a32c7672e2b26c504f38670450
jywarren <jeff@unterbahn.com>
2011-10-02 13:01:31 -0400

    histograms of export resolutions

--- app/models/export.rb
+++ app/models/export.rb
@@ -1,2 +1,36 @@
 class Export < ActiveRecord::Base
+
+	def self.average_cm_per_pixel
+		e = Export.find :all, :conditions => ['cm_per_pixel != "" AND cm_per_pixel < 500'] 
+		sum = 0
+		e.each do |export|
+			sum += export.cm_per_pixel
+		end
+		sum/e.length
+	end
+
+	def self.histogram_cm_per_pixel
+		e = Export.find :all, :conditions => ['cm_per_pixel != "" AND cm_per_pixel < 500'], :order => "cm_per_pixel DESC"
+		hist = []
+		(0..e.first.cm_per_pixel.to_i).each do |bin|
+			hist[bin] = 0
+		end
+		e.each do |export|
+			hist[export.cm_per_pixel.to_i] += 1
+		end
+		hist
+	end
+
+	def self.histogram_cm_per_pixel_in_tens
+		e = Export.find :all, :conditions => ['cm_per_pixel != "" AND cm_per_pixel < 500'], :order => "cm_per_pixel DESC"
+		hist = []
+		(0..(e.first.cm_per_pixel)/10.to_i).each do |bin|
+			hist[bin] = 0
+		end
+		e.each do |export|
+			hist[export.cm_per_pixel/10.to_i] += 1
+		end
+		hist
+	end
+
 end
