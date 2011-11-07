1467b454b94b78d3179c1d5fd28f1b6fb9a7c568
jywarren <jeff@unterbahn.com>
2011-11-06 22:09:06 -0500

    public domain tilesets, 1st attempt

--- public/knitter.js
+++ public/knitter.js
@@ -98,6 +98,13 @@ var Knitter = {
 		} else if (layer == 'yahoo') {
 			var yahoosat = new OpenLayers.Layer.Yahoo("Yahoo Satellite", {type: YAHOO_MAP_SAT, sphericalMercator: true, numZoomLevels: 20});
 			map.addLayer(yahoosat)
+// you can try
+// http://hypercube.telascience.org/tilecache/tilecache.py/1.0.0/NAIP_ALL/
+
+// but you might get better performance from newworld which switches
+// between bmng/landsat/naip based on zoom level
+
+// http://hypercube.telascience.org/tilecache/tilecache.py/1.0.0/NewWorld_google
 		} else if (layer == 'TMS') {
 			Config.tile_url = tile_url || Config.tile_url
 	       		var tms = new OpenLayers.Layer.TMS( "OpenLayers TMS", Config.tile_url,
