832df78df6123ee89cce7745bc05610813a7c976
Justin Manley <manleyjster@gmail.com>
2014-08-01 16:13:34 -0500

    Add left sidebar to main map viewing page.

--- app/controllers/map2_controller.rb
+++ app/controllers/map2_controller.rb
@@ -1,6 +1,10 @@
+require 'open3'
 class Map2Controller < ApplicationController
 	def show 
 		@map = Map.find_by_name(params[:id], :order => 'version DESC')
+
+		@map.zoom = 14
+
 		render :layout => 'knitter'
 	end
 end
