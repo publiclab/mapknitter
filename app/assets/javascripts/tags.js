0e991710fa97bf23bc69b5e75e5b59e26693c722
Justin Manley <manleyjster@gmail.com>
2014-08-15 15:22:36 -0500

    Add dynamic behavior to comment deletion button so that the comment is immediately removed from the page using jQuery (the deletion request is submitted using AJAX, so the page is not reloaded after comment deletion.

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,15 +1,20 @@
 jQuery(document).ready(function($) {
 	/* Enable dynamic comment editing. */
 	$(".edit-comment-btn").click(function(event) {
-		/* 
-		 * Has the form comment_<comment number>, e.g. comment_45s
-		 */
-		var id = event.target.id;
+		var id = $(event.target).data("comment-id");
 
 		/* Hide comment body. */
-		$(".comment-body#body_" + id).toggle();
+		$(".comment-body[data-comment-id=" + id + "]").toggle();
 
 		/* Show comment editing form. */
-		$(".comment-edit-form#edit_" + id).toggle();
+		$(".comment-edit-form[data-comment-id=" + id + "]").toggle();
+	});
+
+	/* Remove comment from the page if */
+	$(".delete-comment-btn").click(function(event) {
+		var id = $(event.target).data("comment-id");
+
+		$(".comment[data-comment-id=" + id + "]").remove();
+		$("#comments-number").text(function(i, str) { return str - 1; }); 
 	});
 });
