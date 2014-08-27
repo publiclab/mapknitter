92d617f5fc238dad1c862bba031e469539a6390b
Justin Manley <manleyjster@gmail.com>
2014-08-27 09:38:46 -0400

    Consolidate header partials into a single file, toggling content based on the context using .fullscree-only and .sidebar-only classes and jQuery.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -3,6 +3,16 @@
 //= require knitter
 //= require mapknitter
 
+jQuery(document).ready(function($) {
+	var sidebarNav = $(".knitter-side-pane .navbar-nav.sidebar-only");
+		navLinks = $(".knitter-side-pane .navbar-nav.fullscreen-only").find("li");
+
+	console.log(sidebarNav);
+	console.log(navLinks);
+
+	sidebarNav.find(".dropdown-menu").append(navLinks);
+});
+
 function addUploadedImageToSidebar($upload) {
 	/* Modify the table row created by jQuery-File-Upload to remove unneeded cells. */
 	$upload.find(".indicate").remove();
