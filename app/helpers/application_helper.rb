1c0f8b674bf532c1c1f60d83010de4adcb9e1d0c
Justin Manley <manleyjster@gmail.com>
2014-09-03 22:52:04 -0400

    Add jquery-ujs (unobtrusive javascript) support and csrf_meta_tags helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,15 @@ module ApplicationHelper
 		APP_CONFIG["password"] == params[:password]
 	end
 
+	# polyfill for jquery-ujs in rails 2.x
+	# see https://github.com/rails/jquery-ujs/wiki/Manual-installing-and-Rails-2
+	def csrf_meta_tags
+		if protect_against_forgery?
+			out = %(<meta name="csrf-param" content="%s"/>\n)
+			out << %(<meta name="csrf-token" content="%s"/>)
+			out % [ Rack::Utils.escape_html(request_forgery_protection_token),
+					Rack::Utils.escape_html(form_authenticity_token) ]
+		end
+ 	end
+
 end
