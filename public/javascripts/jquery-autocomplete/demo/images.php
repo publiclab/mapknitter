80b7a166bdbd0cfb524651ff8b0293dfeb0a104b
jywarren <jeff@unterbahn.com>
2014-12-17 17:23:20 -0500

    removed all non-bowser static assets

--- public/javascripts/jquery-autocomplete/demo/images.php
+++
@@ -1,9 +0,0 @@
-<?php
-$term = $_REQUEST['q'];
-$images = array_slice(scandir("images"), 2);
-foreach($images as $value) {
-	if( strpos(strtolower($value), $term) === 0 ) {
-		echo $value . "\n";
-	}
-}
-?>
