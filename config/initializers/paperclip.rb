30200e875b1668c94a54039990ffa34fb66e2872
jywarren <jeff@unterbahn.com>
2015-02-25 12:30:54 -0500

    image re-added and images paperclip directory fix

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -7,6 +7,6 @@ if Rails.env.production?
   Paperclip::Attachment.default_options[:s3_credentials] = "config/amazon_s3.yml"
 else
   Paperclip::Attachment.default_options[:storage] = :filesystem
-  Paperclip::Attachment.default_options[:path] = ":rails_root/public/:attachment/:id/:style/:filename"
-  Paperclip::Attachment.default_options[:url] = "/:attachment/:id/:style/:filename"
+  Paperclip::Attachment.default_options[:path] = ":rails_root/public/system/:attachment/:id/:style/:filename"
+  Paperclip::Attachment.default_options[:url] = "/system/:attachment/:id/:style/:filename"
 end
