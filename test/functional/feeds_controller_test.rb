cf8a92ffc3fcda2192b7b122c124339816fbdcd3
jywarren <jeff@unterbahn.com>
2018-10-24 09:52:23 -0400

    spam links in moderation feeds

--- test/functional/feeds_controller_test.rb
+++ test/functional/feeds_controller_test.rb
@@ -14,6 +14,18 @@ class FeedsControllerTest < ActionController::TestCase
     assert_not_nil :maps
   end
 
+  test "should get clean feed" do
+    get :all
+    assert_response :success
+    assert_not_nil :maps
+  end
+
+  test "should get clean feed with moderators links" do
+    get :all, :moderators => 'true'
+    assert_response :success
+    assert_not_nil :maps
+  end
+
   test "should get license feed" do
     get :license, :id => "publicdomain"
     assert_response :success
