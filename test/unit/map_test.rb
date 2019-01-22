217004abfbc13512bfa636ac69dbe0457545cb46
jywarren <jeff@unterbahn.com>
2019-01-21 22:59:11 -0500

    map tests

--- test/unit/map_test.rb
+++ test/unit/map_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class MapTest < ActiveSupport::TestCase
- test "Check logged in" do
-   
+
+  test "basics" do
+    map = Map.first
+    assert_not_nil map.license_link
+    assert_not_nil map.grouped_images_histogram(10)
   end
+
+  test "tag basics" do
+    map = Map.first
+    assert !map.has_tag('test')
+    assert map.add_tag('test', User.first)
+    assert map.has_tag('test')
+  end
+
 end
