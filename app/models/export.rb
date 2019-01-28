f7f1ac6b7a02dcd1e3ec4e6e930fd9e3faec4032
jywarren <jeff@unterbahn.com>
2019-01-28 12:19:35 -0300

    fixes

--- app/models/export.rb
+++ app/models/export.rb
@@ -39,7 +39,8 @@ class Export < ActiveRecord::Base
   end
 
   def self.histogram_cm_per_pixel_in_tens
-    e = Export.find :all, :conditions => ['cm_per_pixel != "" AND cm_per_pixel < 500'], :order => "cm_per_pixel DESC"
+    e = Export.where('cm_per_pixel != "" AND cm_per_pixel < 500'])
+      .order(cm_per_pixel: 'desc')
     hist = []
     (0..(e.first.cm_per_pixel)/10.to_i).each do |bin|
       hist[bin] = 0
