1eef816025e2ce8d07b90e17f80e564a10b3328b
jywarren <jeff@unterbahn.com>
2019-01-29 19:01:17 -0600

    output errors?

--- test/unit/map_test.rb
+++ test/unit/map_test.rb
@@ -6,6 +6,19 @@ class MapTest < ActiveSupport::TestCase
     map = Map.first
     assert_not_nil map.license_link
     assert_not_nil map.grouped_images_histogram(10)
+    assert_not_nil map.author
+    assert_not_nil map.name
+    assert_not_nil map.slug
+    assert_not_nil map.lat
+    assert_not_nil map.lon
+    assert_not_nil map.location
+    assert_not_nil map.description
+    assert_not_nil map.zoom
+    assert_not_nil map.license
+    assert_not_nil map.exports
+    assert_not_nil map.tags
+    assert_not_nil map.comments
+    assert_not_nil map.user
   end
 
   test "tag basics" do
--- test/unit/tag_test.rb
+++ test/unit/tag_test.rb
@@ -10,6 +10,9 @@ class TagTest < ActiveSupport::TestCase
     tag = Tag.find_by_name 'nice'
     assert_not_nil tag.name
     assert_not_nil tag.maps
+    assert_equal [], tag.maps
+    assert_not_nil tag.maps.last.has_tag('nice')
+    assert_not_nil tag.maps.last.name
     assert_not_nil tag.maps.last.created_at
   end
 
