1a584c017ba877bd365c740b930bf5cf5c2bdf2c
Jeffrey Warren <jeff@unterbahn.com>
2019-03-21 15:21:07 -0400

    Update cloudnine_install.sh

--- cloudnine_install.sh
+++ cloudnine_install.sh
@@ -1,4 +1,18 @@
-read -p "Enter your cloud9 username: " un
-./install.sh
-rake cloud9 username=$un
-echo "Done! Run the application with 'rails s -b \$IP -p \$PORT'"
+rvm install ruby-2.4.4
+source $(rvm 2.4.4 do rvm env --path)
+rvm use ruby-2.4.4
+gem install rails -v 3.2.0
+sudo apt-get update
+sudo apt-get -y install bundler libmysqlclient-dev imagemagick ruby-rmagick libfreeimage3 libfreeimage-dev ruby-dev libmagickcore-dev libmagickwand-dev
+# exporter-only:
+#sudo apt-get install gdal-bin python-gdal curl libcurl4-openssl-dev libssl-dev zip
+gem install bundler
+bundle _1.16.2_ install
+cp db/schema.rb.example db/schema.rb
+cp config/database.yml.cloud9.example config/database.yml
+sudo service mysql start
+mysql -u root -e 'create database mapknitter;'
+rake db:setup
+sudo apt-get install npm
+sudo npm install -g bower
+bower install
