1679e02240ce5ea57ee6a05f519ecfed987b76eb
Jeffrey Warren <jeff@unterbahn.com>
2019-03-22 16:15:04 -0400

    Update ActiveRecord query syntax to more recent style

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,13 +1,12 @@
 class FeedsController < ApplicationController
 
   def all
-    #(Warpable.all + Map.all).sort_by(&:created_at)
-    @maps = Map.find(:all, 
-      :order => "id DESC",:limit => 20, 
-      :conditions => {:archived => false, :password => ''},
-      :joins => [:user, :warpables], 
-      :group => "maps.id")
-    render :layout => false, :template => "feeds/all"
+    @maps = Map.order(id: :desc)
+      .limit(20)
+      .where(archived: false, password: '')
+      .joins(:user, :warpables)
+      .group("maps.id")
+    render layout: false, template: "feeds/all"
     response.headers["Content-Type"] = "application/xml; charset=utf-8"
   end
 
