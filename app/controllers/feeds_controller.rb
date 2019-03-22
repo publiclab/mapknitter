c70df28899d829f9a38b4b0db7252484cde238ec
Jeffrey Warren <jeff@unterbahn.com>
2019-03-22 16:20:27 -0400

    update syntax of ActiveRecord database query to more recent style

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -12,8 +12,12 @@ class FeedsController < ApplicationController
   end
 
   def clean
-    @maps = Map.find(:all,:order => "id DESC",:limit => 20, :conditions => {:archived => false, :password => ''},:joins => :warpables, :group => "maps.id")
-    render :layout => false, :template => "feeds/clean"
+    @maps = Map.order(id: :desc)
+      .limit(20)
+      .where(archived: false, password: '')
+      .joins(:warpables)
+      .group("maps.id")
+    render layout: false, template: "feeds/clean"
     response.headers["Content-Type"] = "application/xml; charset=utf-8"
   end
 
