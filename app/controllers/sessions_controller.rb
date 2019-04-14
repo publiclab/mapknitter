e78fb7d4719aac87fe2bb4ba936c850d1302f51f
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-04-14 13:58:48 +0530

    Fix layout offences for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,31 +1,31 @@
+# frozen_string_literal: true
+
 require 'uri'
 
-# This controller handles the login/logout function of the site.  
+# This controller handles the login/logout function of the site.
 class SessionsController < ApplicationController
-  #protect_from_forgery :except => [:create]
+  # protect_from_forgery :except => [:create]
 
-  @@openid_url_base  = "https://publiclab.org/people/"
-  @@openid_url_suffix = "/identity"
+  @@openid_url_base = 'https://publiclab.org/people/'
+  @@openid_url_suffix = '/identity'
 
   # render new.erb.html
-  def new #login
+  def new # login
     if logged_in?
-      redirect_to "/"
+      redirect_to '/'
     else
-      @referer = params[:back_to]  
+      @referer = params[:back_to]
     end
   end
 
-  def create #new
+  def create # new
     back_to = params[:back_to]
     open_id = params[:open_id]
     openid_url = URI.decode(open_id)
-    #possibly user is providing the whole URL
-    if openid_url.include? "publiclab"
-      if openid_url.include? "http"
-        url = openid_url
-      end
-    else 
+    # possibly user is providing the whole URL
+    if openid_url.include? 'publiclab'
+      url = openid_url if openid_url.include? 'http'
+    else
       url = @@openid_url_base + openid_url + @@openid_url_suffix
     end
     openid_authentication(url, back_to)
@@ -34,27 +34,27 @@ class SessionsController < ApplicationController
   # only on local installations, to bypass OpenID; add "local: true" to config/config.yml
   # this makes offline development possible; like on a plane! but do NOT leave it open on a production machine
   def local
-    if APP_CONFIG["local"] == true && @current_user = User.find_by_login(params[:login])
-      successful_login '', nil 
+    if APP_CONFIG['local'] == true && @current_user = User.find_by_login(params[:login])
+      successful_login '', nil
     else
-      flash[:error] = "Forbidden"
-      redirect_to "/"
+      flash[:error] = 'Forbidden'
+      redirect_to '/'
     end
   end
 
-  def failed_login(message = "Authentication failed.")
+  def failed_login(message = 'Authentication failed.')
     flash[:danger] = message
     redirect_to '/'
   end
 
   def successful_login(back_to, id)
     session[:user_id] = @current_user.id
-    flash[:success] = "You have successfully logged in."
+    flash[:success] = 'You have successfully logged in.'
     if id
       redirect_to '/sites/' + id.to_s + '/upload'
     else
-      if back_to 
-        redirect_to back_to 
+      if back_to
+        redirect_to back_to
       else
         redirect_to '/sites'
       end
@@ -62,37 +62,37 @@ class SessionsController < ApplicationController
   end
 
   def logout
-    session[:user_id] = nil 
-    flash[:success] = "You have successfully logged out."
+    session[:user_id] = nil
+    flash[:success] = 'You have successfully logged out.'
     redirect_to '/' + '?_=' + Time.now.to_i.to_s
   end
 
   protected
 
   def openid_authentication(openid_url, back_to)
-    #puts openid_url
-    authenticate_with_open_id(openid_url, :required => [:nickname, :email]) do |result, identity_url, registration|
+    # puts openid_url
+    authenticate_with_open_id(openid_url, required: %i[nickname email]) do |result, identity_url, registration|
       if result.successful?
         @user = User.find_by_identity_url(identity_url)
-        if not @user
+        unless @user
           @user = User.new
           @user.login = registration['nickname']
           @user.email = registration['email']
           @user.identity_url = identity_url
-          begin 
+          begin
             @user.save!
           rescue ActiveRecord::RecordInvalid => invalid
             puts invalid
-            failed_login "User can not be associated to local account. Probably the account already exists with different capitalization!" 
+            failed_login 'User can not be associated to local account. Probably the account already exists with different capitalization!'
             return
           end
         end
         nonce = params[:n]
-        if nonce 
+        if nonce
           tmp = Sitetmp.find_by nonce: nonce
-          if tmp 
+          if tmp
             data = tmp.attributes
-            data.delete("nonce")
+            data.delete('nonce')
             site = Site.new(data)
             site.save
             tmp.destroy
@@ -102,7 +102,7 @@ class SessionsController < ApplicationController
         if site
           successful_login back_to, site.id
         else
-          successful_login back_to, nil 
+          successful_login back_to, nil
         end
       else
         failed_login result.message
@@ -110,5 +110,4 @@ class SessionsController < ApplicationController
       end
     end
   end
-
 end
