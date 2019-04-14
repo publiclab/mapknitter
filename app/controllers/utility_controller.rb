4b7161241fbc8c7f3c0c44511114927b7841ce20
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-04-14 13:58:34 +0530

    Fix layout offences for utility controller

--- app/controllers/utility_controller.rb
+++ app/controllers/utility_controller.rb
@@ -1,12 +1,12 @@
-class UtilityController < ApplicationController
+# frozen_string_literal: true
 
+class UtilityController < ApplicationController
   # translates TMS formats from one ordering to another
   def tms_alt
     # /z/x/y.png
     # /z/x/y.png
     # /z/x/(2*z-y-1).png
-    y = 2**params[:z].to_i-params[:y].to_i-1
+    y = 2**params[:z].to_i - params[:y].to_i - 1
     redirect_to "/tms/#{params[:id]}/#{params[:z]}/#{params[:x]}/#{y}.png"
   end
-
 end
