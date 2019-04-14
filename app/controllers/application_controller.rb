3ecd05649f64dbae273018a316d26e70bae3aa96
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-04-14 13:54:56 +0530

    Fix layout offences for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  #include OpenIdAuthentication # shouldn't be necessary!!
+  # include OpenIdAuthentication # shouldn't be necessary!!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?
 
   def current_user
-    user_id = session[:user_id] 
+    user_id = session[:user_id]
     if user_id
       begin
         @user = User.find(user_id)
@@ -24,26 +24,25 @@ class ApplicationController < ActionController::Base
 
   private
 
-    def require_login
-      unless logged_in?
-        path_info = request.env['PATH_INFO']
-        flash[:warning] = "You must be logged in to access this section"
-        redirect_to '/login?back_to=' + path_info.to_param # halts request cycle
-      end
+  def require_login
+    unless logged_in?
+      path_info = request.env['PATH_INFO']
+      flash[:warning] = "You must be logged in to access this section"
+      redirect_to '/login?back_to=' + path_info.to_param # halts request cycle
     end
+  end
 
-    def logged_in?
-      user_id = session[:user_id]
+  def logged_in?
+    user_id = session[:user_id]
 
-      begin
-        if user_id and User.find(user_id)
-          return true
-        else
-          return false
-        end
-      rescue
+    begin
+      if user_id and User.find(user_id)
+        return true
+      else
         return false
       end
+    rescue
+      return false
     end
-
+  end
 end
