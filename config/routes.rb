6f414250f87bba862bb836194283ac2a9f7aaf37
Sasha Boginsky <41092741+sashadev-sky@users.noreply.github.com>
2019-04-15 19:07:19 -0400

    Create Status.json (#483)
    * Export bug fix
    
    * Working export json
    
    * Update example view
    
    * Clean up code
    
    * Clean up code pt2
    
    * Refactor export progress to show status JSON
    
    * Update demo view
    
    * Clean up comments

--- config/routes.rb
+++ config/routes.rb
@@ -100,6 +100,7 @@ Mapknitter::Application.routes.draw do
   get 'maps/:id/warpables' => 'maps#images' # deprecate this in favor of resourceful route below; this is just to override maps/:id
   post 'maps/:map_id/warpables' => 'images#create' # deprecate this in favor of resourceful route below; this is just to override maps/:id
   get 'export/progress/:id' => 'export#progress'
+  get 'export/status/:id' => 'export#status'
   get 'exports' => 'export#index'
   get 'maps/:id' => 'maps#show'
   get 'map/:id', to: redirect('/maps/%{id}')
