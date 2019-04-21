5e725267d258369eecef51a1f8c2505c18288bbc
SidharthBansal <bansal.sidharth2996@gmail.com>
2019-04-22 03:17:08 +0530

    Roles fetched from plots2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ require 'uri'
 class SessionsController < ApplicationController
   #protect_from_forgery :except => [:create]
 
-  @@openid_url_base  = "localhost:3000/people/"
+  @@openid_url_base  = "https://publiclab.org/people/"
   @@openid_url_suffix = "/identity"
 
   # render new.erb.html
