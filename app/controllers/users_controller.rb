61f4e4eb739dca40d559eafad030c17ace3febde
igniteeng000 <igniteeng000@gmail.com>
2019-04-28 16:45:54 +0530

    author sort

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  helper_method :sort_column, :sort_direction
+  
   def profile
     params[:id] = current_user.login if logged_in? && params[:id].zero?
     @user = User.find_by_login(params[:id])
@@ -11,8 +13,18 @@ class UsersController < ApplicationController
     @users = User.joins(:maps)
                  .select('users.*, count(users.id) as maps_count')
                  .group('maps.user_id')
-                 .order('maps_count DESC')
+                 .order(sort_column + ' ' + sort_direction)
                  .paginate(page: params[:page], per_page: 24)
     render 'users/index'
   end
+
+  private
+  def sort_column
+    params[:sort] || "maps_count"
+  end
+  
+  def sort_direction
+    params[:direction] || "desc"
+  end
+
 end
