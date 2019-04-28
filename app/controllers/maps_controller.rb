3fddf0ee8fadad1f9f39c568f21c1d6a6c940803
igniteeng000 <igniteeng000@gmail.com>
2019-04-28 17:35:21 +0530

    Search by author

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -183,9 +183,10 @@ class MapsController < ApplicationController
 
   def search
     params[:id] ||= params[:q]
-    @maps = Map.select('archived, author created_at, description, id, lat, license, location, name, slug, tile_layer, tile_url, tiles, updated_at, user_id, version, zoom')
-               .where('archived = ? AND (name LIKE ? OR location LIKE ? OR description LIKE ?)', false, '%' + params[:id] + '%', '%' + params[:id] + '%', '%' + params[:id] + '%')
-               .paginate(page: params[:page], per_page: 24)
+    @maps = Map.select("archived, author, created_at, description, id, lat, license, location, name, slug, tile_layer, tile_url, tiles, updated_at, user_id, version, zoom")
+             .where('archived = ? AND (author LIKE ? OR name LIKE ? OR location LIKE ? OR description LIKE ?)',
+             false, "%"+params[:id]+"%", "%"+params[:id]+"%", "%"+params[:id]+"%", "%"+params[:id]+"%")
+             .paginate(:page => params[:page], :per_page => 24)
     @title = "Search results for '#{params[:id]}'"
     respond_to do |format|
       format.html { render 'maps/index', layout: 'application' }
