49de9d5e2adda4b71183881f4c66d7dbf83c46ac
sashadev-sky <sboginsky17@gmail.com>
2019-05-27 14:50:55 -0400

    remove unnecessary code

--- config/application.rb
+++ config/application.rb
@@ -61,12 +61,5 @@ module Mapknitter
     config.assets.version = '1.3'
     config.assets.paths << Rails.root.join("public","lib")
 
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
-    end
-
   end
 end
