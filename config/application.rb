e1c92bb5dd060da46db8d2821a49ab56b0c64ea5
sashadev-sky <sboginsky17@gmail.com>
2019-05-28 03:08:47 -0400

    undo new asset path

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Mapknitter
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W[#{config.root}/lib/validators/]
+    config.autoload_paths += %W(#{config.root}/lib/)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
