e36050ac0e80ff46f9d3cf849c9d3f8b849d9a86
Cess <cessmbuguar@gmail.com>
2019-06-04 05:24:16 +0300

    render html for update images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -93,7 +93,7 @@ class ImagesController < ApplicationController
     @warpable.locked = params[:locked]
     @warpable.cm_per_pixel = @warpable.get_cm_per_pixel
     @warpable.save
-    render plain: 'success'
+    render html: 'success'
   end
 
   def destroy
