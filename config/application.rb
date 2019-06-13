d058ce34fd432dc807761ceb7005a71f9131a8db
Alax Alves <alaxallves@gmail.com>
2019-06-13 12:10:35 -0300

    Running update task

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,24 +1,26 @@
 # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.configure do
+    # Version of your assets, change this if you want to expire all your assets.
+    config.assets.version = '1.0'
 
-# Rails.application.config.assets.paths << Emoji.images_path
-# Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+    # Add additional assets to the asset load path.
+    # Rails.application.config.assets.paths << Emoji.images_path
+    # Add Yarn node_modules folder to the asset load path.
+    config.assets.paths << Rails.root.join('public/lib')
+
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+    config.assets.precompile += ['tags.js',
+                               'uploads.js',
+                               'knitter.js',
+                               'annotations.js',
+                               'maps.js',
+                               'admin.js',
+                               'admin.css']
 
 
-
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += ['tags.js',
-                                               'uploads.js',
-                                               'knitter.js',
-                                               'annotations.js',
-                                               'maps.js',
-                                               'admin.js',
-                                               'admin.css']
+    # Precompile additional assets.
+    # application.js, application.css, and all non-JS/CSS in the app/assets
+    # folder are already added.
+    # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+end
