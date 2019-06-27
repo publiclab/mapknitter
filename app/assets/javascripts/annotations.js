a7c7e24831f237a6f56c9f118191b8b1cd976e2b
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-06-27 13:21:18 +0530

    ADS

--- app/assets/javascripts/annotations.js
+++ app/assets/javascripts/annotations.js
@@ -1,3 +1,6 @@
+//= require mapknitter/core/Class
+//= require mapknitter/core/Resources
+//= require mapknitter/Map
 //= require leaflet-draw/dist/leaflet.draw-src.js
 //= require leaflet-illustrate/dist/Leaflet.Illustrate.js
 //= require mapknitter/Annotations
