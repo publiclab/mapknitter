870512c67693c840e4d1d77220601a047a6cb1f5
sashadev-sky <sboginsky17@gmail.com>
2019-06-30 21:58:10 -0400

    rmove extra line

--- install_cloud.sh
+++ install_cloud.sh
@@ -22,5 +22,4 @@ rake db:setup
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get update && sudo apt-get install yarn
-yarn install
 echo "Done! Run the application with 'source start.sh'"
