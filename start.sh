2dcbd4bb71bea3c221cb8530343874122567c88c
Alax Alves <alaxallves@gmail.com>
2019-07-11 17:50:47 -0300

    Precompiling assets before serving

--- start.sh
+++ start.sh
@@ -3,12 +3,11 @@
 pidfile=/app/tmp/pids/server.pid
 
 bundle check || bundle install
-yarn check || yarn install
+rake assets:precompile
 
 if [ -f $pidfile ] ; then
 	>&2 echo 'Server PID file already exists. Removing it...';
 	rm $pidfile;
 fi
 
-#bundle exec /usr/local/bundle/bin/passenger start
 bundle exec passenger start
