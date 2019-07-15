431164dcb4d3c8271146d8f7c5e95e1e9f6830b5
Alax Alves <alaxallves@gmail.com>
2019-07-15 15:42:05 -0300

    Improving Makefile's recipes and target

--- start.sh
+++ start.sh
@@ -10,7 +10,9 @@ bundle check || bundle install
 
 if bundle exec rails db:exists; then
 	>&2 echo "Database exists, only migrating it..."
-	bundle exec rails db:migrate
+	# bundle exec rails db:migrate
+	bundle exec rails db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+	DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bump_database
 else
 	>&2 echo "Database doesn't exist, creating and migrating it..."
 	bundle exec rails db:create
