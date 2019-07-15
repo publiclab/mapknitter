43d86e1486640b239e3ae195a8b8b762266edb92
Alax Alves <alaxallves@gmail.com>
2019-07-15 15:41:28 -0300

    Improving start script

--- start.sh
+++ start.sh
@@ -2,9 +2,23 @@
 
 pidfile=/app/tmp/pids/server.pid
 
+bump_database(){
+	bundle exec rails db:schema:load || bundle exec rails db:migrate
+}
+
 bundle check || bundle install
-rails db:migrate
-rake assets:precompile
+
+if bundle exec rails db:exists; then
+	>&2 echo "Database exists, only migrating it..."
+	bundle exec rails db:migrate
+else
+	>&2 echo "Database doesn't exist, creating and migrating it..."
+	bundle exec rails db:create
+	bump_database
+fi
+
+bundle exec rails assets:precompile
+
 
 if [ -f $pidfile ] ; then
 	>&2 echo 'Server PID file already exists. Removing it...';
