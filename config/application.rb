a2a9a7d81790e1062056481cf803621b5168df13
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-07-18 15:32:10 +0530

    Eager load lib

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Mapknitter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.eager_load_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
