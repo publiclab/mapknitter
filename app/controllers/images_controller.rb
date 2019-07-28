93a78c17d24c3ef5c7d74e1057b0d764dea1dc15
divyabaid16 <divyabaid16@gmail.com>
2019-07-28 14:07:16 +0530

    Small change

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -101,6 +101,7 @@ class ImagesController < ApplicationController
 
   def revert
     @warpable = Warpable.find params[:id]
+    @warpable.count_version = 1
     version = @warpable.versions.find(params[:version])
     version.reify&.save
     redirect_to @warpable.map
