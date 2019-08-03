edaa52254ab7c075d260dd761d76f45710ba699c
divyabaid16 <divyabaid16@gmail.com>
2019-08-03 15:25:16 +0530

    Cloned version added on top

--- app/models/warpable.rb
+++ app/models/warpable.rb
@@ -18,8 +18,7 @@ class Warpable < ActiveRecord::Base
   belongs_to :map
   belongs_to :user
 
-  has_paper_trail on: %i(create update), only: %i(nodes),
-                  unless: proc { |t| t.count_version == 1 }
+  has_paper_trail on: %i(create update), only: %i(nodes)
 
   # overriding JSON formatting for Leaflet.DistortableImage
   def as_json(options = {})
