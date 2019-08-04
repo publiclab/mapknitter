f5679fb8b3e9ec3f36db86a7008c137915003975
Alax Alves <alaxallves@gmail.com>
2019-08-04 13:24:49 -0300

    Adding generated files by webpacker/rails 6

---
+++ config/webpack/environment.js
@@ -0,0 +1,9 @@
+const { environment } = require('@rails/webpacker')
+
+const webpack = require('webpack');
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+}));
+
+module.exports = environment
