d3d17fd61cb8b78f0def12279a6f93d1a84372df
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-08-05 19:24:14 +0530

    Eager load classes while running codecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,7 @@ require 'minitest/reporters'
 MiniTest::Reporters.use! [MiniTest::Reporters::ProgressReporter.new,
                           MiniTest::Reporters::JUnitReporter.new]
 ENV["RAILS_ENV"] = "test"
+Rails.application.eager_load! if ENV['CI']
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
