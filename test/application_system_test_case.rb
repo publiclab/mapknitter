9b650bf61dd37ca38bdfc93b74fc956dfb8bc44a
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-08-13 20:09:25 +0530

    Add chromedriver to travis

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  chromeOptions = %w(--headless  --disable-gpu --no-sandbox --remote-debugging-port=9222)
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => chromeOptions})
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { desired_capabilities: caps }
 end
