f002ad0dbbf689fe81b7b8426b6fbe3b25ebd6e8
viditchitkara <chitkara.vidit@gmail.com>
2019-08-18 18:28:48 +0530

    added a few tests

---
+++ test/system/synchronous_editing_test.rb
@@ -0,0 +1,16 @@
+require 'application_system_test_case'
+
+class SynchronousTest < ApplicationSystemTestCase
+  setup do
+    Capybara.current_driver = Capybara.javascript_driver
+    Capybara.asset_host = "http://localhost:3000"
+  end
+
+  test 'warpables change flow' do
+    map = maps(:saugus)
+    original_data = map.fetch_map_data
+    map.warpables.first.update(nodes: "2,5,1,3")
+    updated_data = map.fetch_map_data
+    assert_not_equal updated_data, original_data
+  end
+end
