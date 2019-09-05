46d1e2efe976b55dc4cb42df1bbd200b2f6cc965
sashadev-sky <sboginsky17@gmail.com>
2019-09-05 01:55:28 -0400

    remove left behind comment

--- db/migrate/20111005211631_add_zip_warpable_res_history.rb
+++ db/migrate/20111005211631_add_zip_warpable_res_history.rb
@@ -3,13 +3,12 @@ class AddZipWarpableResHistory < ActiveRecord::Migration[5.2]
     add_column :warpables, :history, :text, :null => false
     add_column :warpables, :cm_per_pixel, :float, :default => 0, :null => false
     add_column :exports, :zip, :boolean, :default => false, :null => false
-    # Node.find(:all, :conditions => ["description != ''"]).each do |node|
+    
     Warpable.all.each do |w|
       r = w.get_cm_per_pixel
       w.cm_per_pixel = r if r != nil
       w.save
     end
-
   end
 
   def self.down
