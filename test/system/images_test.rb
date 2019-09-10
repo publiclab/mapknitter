ff19414979ea1cf4ee18983a5d9ab56190eb1570
sashadev-sky <sboginsky17@gmail.com>
2019-09-10 00:39:59 -0400

    click_on

--- test/system/images_test.rb
+++ test/system/images_test.rb
@@ -27,7 +27,7 @@ class ImagesTest < ApplicationSystemTestCase
     page.set_rack_session(user_id: @user.id)
 
     visit "/maps/#{@map.slug}"
-    click('Images')
+    click_on "Images"
 
     #expect layers to not have an ldi instance before placing
     assert !page.evaluate_script(check_if_image_placed)
