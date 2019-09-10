cfae26ccb210fd6bf40f6dbfc6b9f26ce6847ba7
sashadev-sky <sboginsky17@gmail.com>
2019-09-09 23:47:34 -0400

    fix

--- app/assets/javascripts/mapknitter/core/Class.js
+++ app/assets/javascripts/mapknitter/core/Class.js
@@ -6,18 +6,15 @@ var MapKnitter = {};
  * See https://github.com/Leaflet/Leaflet/blob/master/src/core/Class.js.
  */
 
-MapKnitter.Class = function() {};
-MapKnitter.Class.extend = function(obj) {
-	var NewClass = function() {
+MapKnitter.Class = function () { };
+MapKnitter.Class.extend = function (obj) {
+	var NewClass = function () {
 		if (this.initialize) {
 			this.initialize.apply(this, arguments);
 		}
-
-		// call all constructor hooks
-		this.callInitHooks();
 	};
 
-	var F = function() {};
+	var F = function () { };
 	F.prototype = this.prototype;
 
 	var proto = new F();
@@ -36,42 +33,8 @@ MapKnitter.Class.extend = function(obj) {
 	var parent = this;
 	NewClass.__super__ = parent.prototype;
 
-	proto._initHooks = [];
-
-	// add method for calling all hooks
-	proto.callInitHooks = function () {
-
-		if (this._initHooksCalled) { return; }
-
-		if (parent.callInitHooks) {
-			parent.callInitHooks.call(this);
-		}
-
-		this._initHooksCalled = true;
-
-		for (var i = 0, len = proto._initHooks.length; i < len; i++) {
-			proto._initHooks[i].call(this);
-		}
-	};
-
 	return NewClass;
 };
-
-
-MapKnitter.Class.include = function(obj) {
+MapKnitter.Class.include = function (obj) {
 	L.extend(this.prototype, obj);
-};
-
-// @function addInitHook(fn: Function): this
-// Adds a [constructor hook](#class-constructor-hooks) to the class.
-MapKnitter.Class.addInitHook = function (fn) { // (Function) || (String, args...)
-	var args = Array.prototype.slice.call(arguments, 1);
-
-	var init = typeof fn === 'function' ? fn : function () {
-		this[fn].apply(this, args);
-	};
-
-	this.prototype._initHooks = this.prototype._initHooks || [];
-	this.prototype._initHooks.push(init);
-	return this;
 };
