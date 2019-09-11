5d0c75373e4ac0d1df9dcd711249a950028f2343
sashadev-sky <sboginsky17@gmail.com>
2019-09-11 01:32:33 -0400

    a

---
+++ db/migrate/20190911052554_restore_open_identity_urls.rb
@@ -0,0 +1,12 @@
+class RestoreOpenIdentityUrls < ActiveRecord::Migration[5.2]
+  def self.up
+    User.all.each do |u|
+      # if it matches http://publiclaboratory.org/...
+      if u.identity_url != "" && !u.identity_url.nil? && u.identity_url[0..26] == "http://publiclaboratory.org"
+        u.identity_url = "http://publiclab.org/openid/"+ u.login.downcase
+        puts " => "+ u.identity_url
+        u.save
+      end
+    end
+  end
+end
