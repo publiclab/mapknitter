c14a5b094eab3b453d2ad309a6be33cb885326c5
Sebastian Silva <sebastian@fuentelibre.org>
2020-04-23 17:55:35 -0400

    Precompile on first load, get dependencies at build time.

--- start.sh
+++ start.sh
@@ -7,27 +7,12 @@ if [ ! -f "./config/initializers/recaptcha.rb" ]; then
     echo -e "\e[94mRead More\e[0m: https://github.com/publiclab/mapknitter#installation-steps"
 fi
 
-bump_database(){
-	bundle exec rails db:schema:load || bundle exec rails db:migrate
-}
 
-bundle check || bundle install
-
-if bundle exec rails db:exists; then
-	>&2 echo "Database exists, only migrating it..."
-	bundle exec rails db:migrate
-else
-	>&2 echo "Database doesn't exist, creating and migrating it..."
-	bundle exec rails db:create
-	bump_database
-fi
-
-bundle exec rails assets:precompile
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails assets:precompile
 
 if [ -f $pidfile ] ; then
 	>&2 echo 'Server PID file already exists. Removing it...';
 	rm $pidfile;
 fi
 
-# ? forego start
 bundle exec rails s -p $PORT -b '0.0.0.0'
