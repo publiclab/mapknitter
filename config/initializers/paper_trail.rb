ce1a97c752d5518b9586675a067f7d011255121e
Jeffrey Warren <jeff@unterbahn.com>
2020-05-19 13:37:51 -0400

    PaperTrail.config.version_limit = 20 (#1312)
    cc #1167

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,1 +1,5 @@
 PaperTrail.serializer = PaperTrail::Serializers::JSON
+
+# https://github.com/paper-trail-gem/paper_trail#2e-limiting-the-number-of-versions-created
+# Limit: 21 versions per record (20 most recent, plus a `create` event)
+PaperTrail.config.version_limit = 20
