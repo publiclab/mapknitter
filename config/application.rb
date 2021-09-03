ce1621ec2a9404a579307b9d2cfff0c4d5626a0e
Alicia Paz <aliciapazrojas@gmail.com>
2021-09-02 20:30:59 -0400

    Introduce Gitpod to migration work (#1538)
    * Add CORS headers (#1536)
    
    * Create .gitpod.dockerfile
    
    * Create .gitpod.yml
    
    * Create database.yml.gitpod
    
    * Update .gitpod.yml
    
    * ruby 2.4.6 in gitpod.dockerfile
    
    * Update .gitpod.yml
    
    * Update database.yml.gitpod
    
    Co-authored-by: Sebastian Silva <sebastian@fuentelibre.org>
    Co-authored-by: Jeffrey Warren <jeff@unterbahn.com>

--- config/application.rb
+++ config/application.rb
@@ -46,6 +46,8 @@ module Mapknitter
     config.assets.enabled = true
 
     config.action_dispatch.default_headers['X-Frame-Options'] = "ALLOW-FROM https://publiclab.org"
+    config.action_dispatch.default_headers['Access-Control-Allow-Origin'] = 'https://publiclab.org',
+    config.action_dispatch.default_headers['Access-Control-Request-Method'] = 'GET' 
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.paths << Rails.root.join("public","lib")
