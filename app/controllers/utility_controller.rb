b1df7f01531a3cc3cefcc6f1831254f75091af8a
Jeffrey Warren <jeff@unterbahn.com>
2021-11-02 11:11:09 -0700

    Update app/controllers/utility_controller.rb

--- app/controllers/utility_controller.rb
+++ app/controllers/utility_controller.rb
@@ -5,7 +5,7 @@ class UtilityController < ApplicationController
   # if no static local file exists in format /tms/:id/:z/:x/:y.png, this action handles the request
   def tms
     unless Map.find_by(slug: params[:id]) # if no map with the given slug is found, record_not_found is thrown
-      redirect_to "https://mapknitter-exports-warps.storage.googleapis.com/#{params[:id]}/tms/#{params[:z]}/#{params[:x]}/#{params[:y]}.png"
+      redirect_to("https://mapknitter-exports-warps.storage.googleapis.com/#{params[:id]}/tms/#{params[:z]}/#{params[:x]}/#{params[:y]}.png")
     end
   end
 
