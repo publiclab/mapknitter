bc7a4594e7e08bda95522351c8e5dc8ed472ee95
Jeffrey Warren <jeff@unterbahn.com>
2021-11-03 12:49:41 -0700

    Adjust TMS redirects for google cloud, new route redirect style (#1548)
    * Adjust TMS redirects for google cloud, new route redirect style
    
    * Update utility_controller.rb
    
    * Apply suggestions from code review
    
    * Update _cloud_exports.html.erb
    
    * Update tms_info.html.erb
    
    * Delete utility_controller_test.rb
    
    * Update routes.rb
    
    * Update utility_controller.rb
    
    * Update app/controllers/utility_controller.rb
    
    * Update utility_controller.rb
    
    * Update .codecov.yml
    
    * Update _cloud_exports.html.erb

--- app/controllers/utility_controller.rb
+++ app/controllers/utility_controller.rb
@@ -1,10 +1,11 @@
 class UtilityController < ApplicationController
-  # translates TMS formats from one ordering to another
-  def tms_alt
-    # /z/x/y.png
-    # /z/x/y.png
-    # /z/x/(2*z-y-1).png
-    y = 2**params[:z].to_i - params[:y].to_i - 1
-    redirect_to("/tms/#{params[:id]}/#{params[:z]}/#{params[:x]}/#{y}.png")
+  # TMS redirects now handled mostly in config/routes.rb
+
+  # most export TMS routes are handled in app/views/maps/_cloud_exports.html.erb, but this may catch some too
+  # if no static local file exists in format /tms/:id/:z/:x/:y.png, this action handles the request
+  def tms
+    unless Map.find_by(slug: params[:id]) # if no map with the given slug is found, record_not_found is thrown
+      redirect_to("https://mapknitter-exports-warps.storage.googleapis.com/#{params[:id]}/tms/#{params[:z]}/#{params[:x]}/#{params[:y]}.png")
+    end
   end
 end
