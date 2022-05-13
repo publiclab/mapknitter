048756533fbdd8f6e9c68ae6f8830d20a6ef6794
Jeffrey Warren <jeff@unterbahn.com>
2022-05-13 17:21:58 -0400

    system test for map location autocompletion (#1697)
    * system test for map location autocompletion
    
    * change parallel test syntax
    
    * nanasess/setup-chromedriver@v1.0.7
    
    * add more chromedriver stuff
    
    * test adjustment to wait for result
    
    * change path for spin.js/spin.min.js
    
    * catch origin validator nil
    
    * path fix
    
    * click on New Haven
    
    * Update maps_test.rb

--- lib/not_at_origin_validator.rb
+++ lib/not_at_origin_validator.rb
@@ -4,7 +4,7 @@ require 'open3'
 class NotAtOriginValidator < ActiveModel::Validator
   def validate(record)
     # adding exception to maps created before this validation was added: https://github.com/publiclab/mapknitter/issues/1264
-    if null_island?(record) && record.created_at > DateTime.new(2015, 1, 13, 0, 0, 0)
+    if null_island?(record) && !record.created_at.nil? && record.created_at > DateTime.new(2015, 1, 13, 0, 0, 0)
       record.errors[:base] << '0,0 is an unlikely locale.'
     end
   end
---
+++ test/system/maps_test.rb
@@ -0,0 +1,49 @@
+require "application_system_test_case"
+
+class MapsTest < ApplicationSystemTestCase
+  def setup
+    @user = User.create({login: Faker::Internet.username,
+                     name: Faker::Name.name,
+                     email: Faker::Internet.email})
+    # @map = Map.create(
+    #     name: Faker::Address.city,
+    #     lat: Faker::Address.latitude,
+    #     lon: Faker::Address.longitude,
+    #     location: Faker::Address.country,
+    #     description: Faker::Lorem.sentence,
+    #     slug: Faker::Lorem.word,
+    #     user: @user,
+    #     author: @user.login
+    #   )
+  end
+
+  test "Map creation form" do
+
+    #log in 
+    page.set_rack_session(user_id: @user.id)
+
+    visit "/maps/new"
+
+    # expect at least one result
+    assert page.evaluate_script("$('.ui-menu-item').length == 0")
+
+    fill_in('map_name', with: 'New Haven Map')
+
+    fill_in('map_location', with: 'New Haven')
+
+    assert_selector('.ui-menu-item', text: 'New Haven, CT, USA') # if geocoding works
+
+    # expect at least one result
+    assert page.evaluate_script("$('.ui-menu-item').length >= 1")
+    
+    find('.ui-menu-item').click
+
+    click_on "Create map"
+
+    assert_selector('h3#map_title', text: "New Haven Map")
+    
+    assert page.evaluate_script("parseInt($('#map_lat').val()) == 41")
+    assert page.evaluate_script("parseInt($('#map_lon').val()) == -72")
+  end
+
+end
