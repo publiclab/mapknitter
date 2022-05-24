ed166fe61b6e5ec70ec504bc69de8e2ec9563daa
jywarren <jeff@unterbahn.com>
2022-05-24 21:44:26 +0000

    more gdal removal

--- lib/exporter-deps.sh
+++ lib/exporter-deps.sh
@@ -2,12 +2,9 @@ sudo add-apt-repository -y ppa:ubuntugis/ppa
 sudo apt-get update
 
 # Installing Mapknitter dependencies
-sudo apt-get install -y gdal-bin \
-                        python3-gdal \
-                        python-gdal \
-                        curl \
+sudo apt-get install -y curl \
                         libcurl4-openssl-dev \
-                        libssl-dev zip \
+                        libssl-dev \
                         libmysqlclient-dev \
                         imagemagick \
                         ruby-rmagick \
