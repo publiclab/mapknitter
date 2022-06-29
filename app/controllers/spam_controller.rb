e23d05694ac934db461b93b42f17aa36fa3b1071
PeculiarE <perhisohwode@gmail.com>
2022-06-29 22:00:52 +0100

    refactor: 💡 discarded authors' object and used count instead

--- app/controllers/spam_controller.rb
+++ app/controllers/spam_controller.rb
@@ -1,29 +1,27 @@
 class SpamController < ApplicationController
   module ModerationGuards
     def spam_check(map)
-      #check and spam only unspammed maps
+      # check and spam only unspammed maps
       map.spam unless map.status == Map::Status::BANNED
     end
   
     def ban_check(map)
-      #check and ban only unbanned non-anonymous authors
+      # check and ban only unbanned non-anonymous authors
       map.user.ban unless map.anonymous? || map.user.status == User::Status::BANNED
     end
 
     def publish_check(map)
-      #check and publish only spammed or moderated maps
+      # check and publish only spammed or moderated maps
       map.publish unless map.status == Map::Status::NORMAL
     end
 
     def unban_check(map)
-      #check and unban only banned non-anonymous authors
+      # check and unban only banned non-anonymous authors
       map.user.unban unless map.anonymous? || map.user.status != User::Status::BANNED
     end
   end
 
   include ModerationGuards
-
-  require 'set'
 
   before_action :require_login
   before_action { logged_in_as(['admin', 'moderator'], 'moderate maps and users') }
@@ -42,15 +40,15 @@ class SpamController < ApplicationController
 
   def batch_spam_maps
     spammed_maps = 0
-    banned_authors = Set.new
+    banned_authors = 0
     params[:ids].split(',').uniq.each do |id|
       map = Map.find(id)
       if spam_check(map)
         spammed_maps += 1
-        banned_authors << map.user.id if ban_check(map)
+        banned_authors += 1 if ban_check(map)
       end
     end
-    flash[:notice] = helpers.pluralize(spammed_maps, 'map') + ' spammed and ' + helpers.pluralize(banned_authors.size, 'author') + ' banned.'
+    flash[:notice] = helpers.pluralize(spammed_maps, 'map') + ' spammed and ' + helpers.pluralize(banned_authors, 'author') + ' banned.'
     redirect_back(fallback_location: root_path)
   end
 
@@ -68,15 +66,15 @@ class SpamController < ApplicationController
 
   def batch_publish_maps
     published_maps = 0
-    unbanned_authors = Set.new
+    unbanned_authors = 0
     params[:ids].split(',').uniq.each do |id|
       map = Map.find(id)
       if publish_check(map)
         published_maps += 1
-        unbanned_authors << map.user.id if unban_check(map)
+        unbanned_authors += 1 if unban_check(map)
       end
     end
-    flash[:notice] = helpers.pluralize(published_maps, 'map') + ' published and ' + helpers.pluralize(unbanned_authors.size, 'author') + ' unbanned.'
+    flash[:notice] = helpers.pluralize(published_maps, 'map') + ' published and ' + helpers.pluralize(unbanned_authors, 'author') + ' unbanned.'
     redirect_back(fallback_location: root_path)
   end
 end
